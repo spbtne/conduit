360c967292f369c19f37071caeaa3e60e879b12e
Nikita Tushin <nikita_tushin@epam.com>
2022-01-19 17:32:26 +0300

    add: register markup, route, form

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
+
+import { RegisterComponent } from './components/register/register.component';
+
+const routes: Routes = [
+  {
+    path: 'register',
+    component: RegisterComponent,
+  },
+];
+
+@NgModule({
+  declarations: [RegisterComponent],
+  imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes)],
+})
+export class AuthModule {}
