4ac0e7c0a73dce585299204858b6ccb6d1d7ed0d
Nikita Tushin <nikita_tushin@epam.com>
2022-02-02 16:32:19 +0300

    add: loading dots animation

--- src/app/shared/modules/spinner/component/spinner/spinner.component.ts
+++ src/app/shared/modules/spinner/component/spinner/spinner.component.ts
@@ -2,7 +2,13 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-spinner',
-  template: `<div class="text-success">Loading...</div>`,
+  template: `<div class="text-success">
+    Loading
+    <span class="dot-1">.</span>
+    <span class="dot-2">.</span>
+    <span class="dot-3">.</span>
+  </div>`,
+  styleUrls: ['./spinner.component.scss'],
 })
 export class SpinnerComponent implements OnInit {
   constructor() {}
